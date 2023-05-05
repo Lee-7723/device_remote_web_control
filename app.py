a47bc50d81bf1275d0dfc111d34acf46faea0601
Lee-7723 <1981671849@qq.com>
2023-05-05 12:04:38 +0800

    merging

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def hello_world():
 def var(num):
     return f'var {num}'
 
-@app.route('/power/status/')
+@app.route('/power/status/')    #查看power开关状态接口
 def check_power_status():
     response = {
     'state': N6700C.querystates(), 
@@ -35,44 +35,30 @@ def check_power_status():
 
 @app.route('/power/', methods=['POST', 'GET'])
 def power():
-    # if request.method == 'POST':
-    #     if request.form['status'] == 'on':
-    #         N6700C.poweron()
-    #     else:
-    #         N6700C.poweroff()
-    # else: pass
-
     if request.method == 'POST':
-        print(int(request.form['cycle']))
+        #print(int(request.form['cycle']))
         cycle = int(request.form['cycle'])
         voltage = float(request.form['voltage'])
         uptime = float(request.form['uptime'])
         downtime = float(request.form['downtime'])
-        a = N6700C.setPowercycle.delay(cycle, voltage, uptime, downtime)
+        a = N6700C.setPowercycle.delay(cycle, voltage, uptime, downtime)    #celery用法，将任务交给后台，delay是apply_async的快捷方式
         #a = setCycle.apply_async(args=[cycle, voltage, uptime, downtime])
         print(a.id)
         print(a.status)
     
     if N6700C.queryerrors()==False and 'a' in locals().keys():
         #print(N6700C.querystates())
-        response = make_response()
-        response.status = '302'
-        response.headers['Location'] = '/static/Power.html?'+a.id
-        response.headers['task_id'] = a.id
-        response.headers['test'] = 'test'
-        # return response
         task = {'task_id': a.id}
-        return render_template('Power.html', task=task, state=N6700C.queryerrors())
     else:
         #print(N6700C.querystates())
         task = {'task_id': 'no task running'}
-        return render_template('Power.html', task=task, state=N6700C.queryerrors())
+    return render_template('Power.html', task=task, state=N6700C.queryerrors()) #向浏览器返回html响应，并在返回的响应中记录task_id，方便标记任务，同时用于浏览器查询任务状态
     
-@celery.task(bind=True)#准备弃用，celery加在具体函数里
-def setCycle(self, cycle, voltage, uptime, downtime):
-    N6700C.setPowercycle(cycle, voltage, uptime, downtime)
+# @celery.task(bind=True)#已弃用，celery加在具体函数所在的py文件里
+# def setCycle(self, cycle, voltage, uptime, downtime):
+#     N6700C.setPowercycle(cycle, voltage, uptime, downtime)
 
-@app.route('/task_status/<task_id>/')#任务状态查询接口
+@app.route('/task_status/<task_id>/')   #任务状态查询接口
 def queryTaskstatus(task_id):
     #print('url = '+task_id)
     status = AsyncResult(id=task_id, app=celery)
@@ -86,16 +72,16 @@ def queryTaskstatus(task_id):
         response = {
             'state': status.state
         }
-    running_tasks = celery.control.inspect().active()['celery@CAGR4NXRAO']
+    running_tasks = celery.control.inspect().active()['celery@CAGR4NXRAO']  #他人登入时可以看Current task状态来判断仪器是否被占用。
     if len(running_tasks)!=0:
-        print(running_tasks[0]['name'])
+        #print(running_tasks[0]['name'])
         response['current_task'] = running_tasks[0]['name']
     else:
-        print('no task running')
+        #print('no task running')
         response['current_task'] = 'no task'
-    return jsonify(response)
+    return jsonify(response)    #以json形式返回数据，网页中用ajax向此接口进行查询
 
-@app.route('/kill_task/<task_id>')
+@app.route('/kill_task/<task_id>')  #终止任务接口
 def killTask(task_id):
     task = AsyncResult(id=task_id, app=celery)
     task.revoke(terminate=True)
